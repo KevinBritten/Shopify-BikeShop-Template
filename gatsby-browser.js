1ba81d052d79ee57b54d3b5a82cb25780675c9ec
Kevin Britten <kevinjbritten@gmail.com>
2023-05-26 17:33:10 -0400

    wip

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,8 @@ import "./src/styles/reset.css"
 import "./src/styles/variables.css"
 import "./src/styles/global.css"
 
+import "@splidejs/splide/dist/css/themes/splide-default.min.css"
+
 export const wrapRootElement = ({ element }) => (
   <StoreProvider>{element}</StoreProvider>
 )
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,53 @@
-import * as React from "react"
+// Hero.js
+import React from "react"
+import { Splide, SplideSlide } from "@splidejs/react-splide"
+import Slide from "./Slide"
 
-function Hero(props) {
-  return (
-    <div>
-      <h1>Courrier Caverne</h1>
-    </div>
-  )
-}
+const slides = [
+  // Your slide data here
+  {
+    image: "../images/placeholder-img-1.png",
+    title: "Slide 1 Title",
+    subtitle: "Slide 1 Subtitle",
+    target: "/target1",
+  },
+  {
+    image: "../images/placeholder-img-2.png",
+    title: "Slide 1 Title",
+    subtitle: "Slide 1 Subtitle",
+    target: "/target1",
+  },
+  {
+    image: "../images/placeholder-img-3.png",
+    title: "Slide 1 Title",
+    subtitle: "Slide 1 Subtitle",
+    target: "/target1",
+  },
+  // More slides...
+]
+
+const Hero = () => (
+  <Splide
+    options={{
+      type: "loop",
+      gap: "1rem",
+      pagination: true,
+      autoplay: true,
+      pauseOnHover: false,
+      resetProgress: false,
+    }}
+  >
+    {slides.map((slide, index) => (
+      <SplideSlide key={index}>
+        <Slide
+          imageSrc={slide.image}
+          title={slide.title}
+          subtitle={slide.subtitle}
+          targetSection={slide.target}
+        />
+      </SplideSlide>
+    ))}
+  </Splide>
+)
 
 export default Hero
