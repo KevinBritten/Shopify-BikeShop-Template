f53cf14363526b51c31869bb2218488972a03110
Kevin Britten <kevinjbritten@gmail.com>
2023-06-05 12:53:26 -0400

    edit footer for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react"
 
 function Footer() {
   return (
-    <footer className="bg-black px-4 lg:px-16 py-8 flex justify-between items-center text-white">
-      <ul>
+    <footer className="bg-black px-4 lg:px-16 py-8 flex justify-center md:justify-between items-center text-white">
+      <ul className="hidden md:block">
         <li>7400 rue Saint-André</li>
         <li>Montréal, Québec</li>
         <li>H2R 2P7</li>
@@ -67,7 +67,10 @@ function Footer() {
           </svg>
         </a>
       </div>
-      <a href="mailto:info@courriercaverne.ca" className="underline">
+      <a
+        href="mailto:info@courriercaverne.ca"
+        className="underline hidden md:block"
+      >
         info@courriercaverne.ca
       </a>
     </footer>
