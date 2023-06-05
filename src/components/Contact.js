3035275e3c26611290767ccb6bcab55c68bc71ff
Kevin Britten <kevinjbritten@gmail.com>
2023-06-05 12:50:41 -0400

    edit contact for mobile

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,10 +12,10 @@ function Contact({ language }) {
     <section id={t("links.contact")} className="bg-light-bg px-4 py-8 lg:p-16">
       <h1 className="text-center">{t("contact.title")}</h1>
       <div
-        className="grid grid-cols-2 auto-rows-auto mt-8"
+        className="grid md:grid-cols-2 mt-8"
         style={{ gridTemplateColumns: "1fr auto" }}
       >
-        <div className="row-span-3 pr-4 lg:pr-8">
+        <div className="row-span-3 col-span-2 md:col-span-1 row-start-4 md:row-start-1 mt-4 md:mt-auto md:pr-4 lg:pr-8 h-96 md:h-auto">
           <iframe
             src={
               `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.387256282514!2d-73.6189176226199!3d45.542534771075466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc9193dfb48aaf1%3A0x12cea4ffb76f509b!2s7400%20St%20Andre%20St%2C%20Montreal%2C%20QC%20H2R%202P7!5e0!3m2!1s` +
@@ -31,7 +31,7 @@ function Contact({ language }) {
           ></iframe>
           <div></div>
         </div>
-        <div>
+        <div className="col-span-2 md:col-span-1 text-center md:text-left">
           <h2 className="text-compliment">{t("contact.hours.title")}</h2>
           <ul>
             <li>{t("contact.hours.days.0")}: 9am - 6pm</li>
@@ -45,7 +45,7 @@ function Contact({ language }) {
             </li>
           </ul>
         </div>
-        <div>
+        <div className="col-span-2 md:col-span-1 text-center md:text-left">
           <h2 className="text-compliment">{t("contact.location.title")}</h2>
           <ul>
             <li>7400 rue Saint-André</li>
@@ -53,7 +53,7 @@ function Contact({ language }) {
             <li>H2R 2P7</li>
           </ul>
         </div>
-        <div>
+        <div className="col-span-2 md:col-span-1 text-center md:text-left">
           <h2 className="text-compliment">{t("contact.contact.title")}</h2>
           <ul>
             <li>
