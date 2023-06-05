381bdb0889e5865f8a3dc7641c7ebfff271b4986
Kevin Britten <kevinjbritten@gmail.com>
2023-06-05 12:31:18 -0400

    add padding intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,7 +8,7 @@ function Intro({ language }) {
     i18n.changeLanguage(language)
   }, [i18n, language])
   return (
-    <section className="text-center pt-14 pb-20">
+    <section className="text-center pt-14 pb-20 px-4">
       <h1>{t("intro.title")}</h1>
       <p className="m-auto max-w-2xl py-4">{t("intro.body")}</p>
     </section>
