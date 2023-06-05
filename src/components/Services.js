3c1419606999c988ecd70094d0534c2ce09590a8
Kevin Britten <kevinjbritten@gmail.com>
2023-06-05 12:34:41 -0400

    adjust services for mobile

--- src/components/Services.js
+++ src/components/Services.js
@@ -50,7 +50,7 @@ function Services({ language }) {
       <div className="px-4 py-8 lg:p-16 bg-light-bg">
         <h1 className="text-center">{t("services.title")}</h1>
         <h2 className="text-center mt-8">{t("services.subtitle")}</h2>
-        <div className="grid grid-cols-3 gap-4 border-compliment mt-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4  mt-4">
           {serviceLevels.map((level, index) => (
             <ServicesCard
               key={index}
@@ -68,7 +68,7 @@ function Services({ language }) {
           alt={"A picture of our workbench"}
           className="relative w-full h-full"
         />
-        <div className="lg:w-full lg:max-w-3xl absolute top-1/2 lg:left-1/2 transform lg:-translate-x-1/2 -translate-y-1/2 px-16 py-8 bg-white bg-opacity-90 flex flex-col items-center justify-between">
+        <div className="lg:w-full lg:max-w-3xl absolute top-1/2 lg:left-1/2 transform lg:-translate-x-1/2 -translate-y-1/2 px-4 md:px-16 py-8 bg-white bg-opacity-90 flex flex-col items-center justify-between">
           <h2 className=" text-compliment">{t("services.estimate.title")}</h2>
           <p className=" max-w-2xl mt-4">{t("services.estimate.body")}</p>
         </div>
